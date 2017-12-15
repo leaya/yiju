e86d3d670c710c7b520918e1b7411db87badcf9a
lihaoxin <331926815@qq.com>
2017-12-15 02:35:27 +0000

    update category url 301 redirect

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,8 +8,12 @@ use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    public function index(Category $category)
+    public function index(Category $category, Request $request)
     {
+        if ( !empty($category->slug) && $category->slug != $request->slug) {
+            return redirect(route('categories.index', array_merge([$category->id, $category->slug])), 301);
+        }
+
         $categories = Category::where('parent_id', null)->with('subCat')->Orderby('order')->get();
         $posts = Post::with('authorId')->where('category_id', $category->id)->orderBy('created_at', 'desc')->paginate(5);
         return view('categories.index', compact('categories', 'posts'))->with('currentCat', $category);
